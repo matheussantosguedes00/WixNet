23805011c3e316bc58d2ba6c8a5cc81e08b90677
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2023-07-26 15:08:43 -0300

    26/07/2023

--- MenuInicial/Projeto1/src/app/components/pages/painel-controle/painel-controle.component.ts
+++ MenuInicial/Projeto1/src/app/components/pages/painel-controle/painel-controle.component.ts
@@ -40,6 +40,24 @@ export class PainelControleComponent {
         title: 'Card 4', 
         description: 'Descrição do Card 4',
         content: 'Conteúdo do Card 4'
+      },
+      { 
+        id: 5, 
+        title: 'Card 5', 
+        description: 'Descrição do Card 5',
+        content: 'Conteúdo do Card 5'
+      },
+      { 
+        id: 6, 
+        title: 'Card 6', 
+        description: 'Descrição do Card 6',
+        content: 'Conteúdo do Card 6'
+      },
+      { 
+        id: 7, 
+        title: 'Card Maior', 
+        description: 'Descrição do Card 7',
+        content: 'Conteúdo do Card 7'
       }
     ];
 
