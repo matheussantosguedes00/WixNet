759fff53c2de3d1fc7a88d25b32922f50924829b
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2023-07-28 12:22:53 -0300

    28/07/2023

--- MenuInicial/Projeto1/src/app/components/pages/login/auth.service.ts
+++ MenuInicial/Projeto1/src/app/components/pages/login/auth.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   private isAuthenticated: boolean = false;
+  private tokenKey: string = 'meu_token_de_acesso'; // Chave para o token no LocalStorage
   private errorMessage: string = '';
 
   login(username: string, password: string): boolean {
@@ -12,24 +13,36 @@ export class AuthService {
     // Neste exemplo, faremos uma autenticação simples verificando se o username é "admin" e a senha é "123"
     if (username === 'admin' && password === '123') {
       this.isAuthenticated = true;
+      this.saveToken('seu_token_de_acesso_aqui'); // Salvar o token no LocalStorage após o login
       return true;
     } else {
       this.isAuthenticated = false;
-      this.errorMessage = 'Credenciais inválidas. Verifique seu Email e Senha.';
       return false;
     }
   }
 
   isLoggedIn(): boolean {
-    return this.isAuthenticated;
+    // Verificar se o token está presente no LocalStorage e se o usuário está autenticado
+    return this.getToken() !== null && this.isAuthenticated;
   }
 
   logout(): void {
     this.isAuthenticated = false;
+    this.removeToken(); // Remover o token do LocalStorage ao fazer logout
   }
 
+  private saveToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  private removeToken(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
   getErrorMessage(): string {
-    
     return this.errorMessage;
   }
 }
