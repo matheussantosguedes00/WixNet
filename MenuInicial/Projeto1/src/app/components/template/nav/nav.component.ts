8620957769e1154ebb03421bc2bfceb04bdcbbc1
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2023-08-07 10:49:22 -0300

    07-08

--- MenuInicial/Projeto1/src/app/components/template/nav/nav.component.ts
+++ MenuInicial/Projeto1/src/app/components/template/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
   activateIcon(iconIndex: number) {
     this.activeIcon = iconIndex;
     localStorage.setItem('activeIcon', JSON.stringify(iconIndex));
-   
+  
   }
 
 
