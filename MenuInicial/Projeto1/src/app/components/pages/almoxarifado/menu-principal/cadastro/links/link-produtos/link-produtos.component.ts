5f74797555b46f7bc4ba691a431c7cacac44ccc3
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2023-08-10 16:59:31 -0300

    10-08

--- MenuInicial/Projeto1/src/app/components/pages/almoxarifado/menu-principal/cadastro/links/link-produtos/link-produtos.component.ts
+++ MenuInicial/Projeto1/src/app/components/pages/almoxarifado/menu-principal/cadastro/links/link-produtos/link-produtos.component.ts
@@ -7,24 +7,68 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LinkProdutosComponent {
   formulario: FormGroup;
-
+  produtos: any[] = [];
+  produtoParaEditar: any = null; // Inicialize como null para não preencher o formulário por padrão
   constructor(private fb: FormBuilder) {
     this.formulario = this.fb.group({
-      item: ['', Validators.required],
-      codigoProduto: ['', Validators.required],
-      unidadeMedida: ['', Validators.required],
-      estoqueMinimo: ['', Validators.required],
-      custoUnitario: ['', Validators.required],
-      km: ['', Validators.required],
-      modelo: ['', Validators.required],
-      serialNumero: ['', Validators.required]
+      item: [''],
+      km: [''],
+      estoqueMinimo: [''],
+      codigoProduto: [''],
+      modelo: [''],
+      custoUnitario: [''],
+      unidadeMedida: [''],
+      serialNumero: ['']
     });
+
+    // Adicionar alguns produtos à lista como exemplo
+    this.produtos.push(
+      {
+        item: 'Produto 1',
+        km: 100,
+        estoqueMinimo: 10,
+        codigoProduto: 'P123',
+        modelo: 'Modelo A',
+        custoUnitario: 50.00,
+        unidadeMedida: 'Unidade',
+        serialNumero: 'SN123'
+      },
+      {
+        item: 'Produto 2',
+        km: 200,
+        estoqueMinimo: 20,
+        codigoProduto: 'P456',
+        modelo: 'Modelo B',
+        custoUnitario: 70.00,
+        unidadeMedida: 'Unidade',
+        serialNumero: 'SN456'
+      }
+      // Adicione mais produtos se necessário
+    );
+  }
+
+ 
+
+  editarProduto(index: number) {
+    this.produtoParaEditar = this.produtos[index];
+    this.formulario.patchValue(this.produtoParaEditar);
   }
 
   enviarFormulario() {
     if (this.formulario.valid) {
-      console.log(this.formulario.value);
-      // Aqui você pode fazer a lógica para enviar os dados para o servidor, por exemplo.
+      if (this.produtoParaEditar !== null) {
+        const index = this.produtos.indexOf(this.produtoParaEditar);
+        this.produtos[index] = this.formulario.value;
+        this.produtoParaEditar = null;
+      } else {
+        this.produtos.push(this.formulario.value);
+      }
+      this.formulario.reset();
     }
   }
+  excluirProduto(index: number) {
+    this.produtos.splice(index, 1);
+  }
 }
+ 
+
