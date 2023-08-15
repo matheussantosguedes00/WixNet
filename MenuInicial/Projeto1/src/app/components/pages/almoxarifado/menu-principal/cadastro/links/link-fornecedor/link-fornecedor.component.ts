42abdc9180826821c9f14e2da6561b84fae04e43
Matheus Santos Guedes <matheussantosguedes99@gmail.com>
2023-08-15 13:52:16 -0300

    15-08

--- MenuInicial/Projeto1/src/app/components/pages/almoxarifado/menu-principal/cadastro/links/link-fornecedor/link-fornecedor.component.ts
+++ MenuInicial/Projeto1/src/app/components/pages/almoxarifado/menu-principal/cadastro/links/link-fornecedor/link-fornecedor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fornecedor',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./link-fornecedor.component.css']
 })
 export class LinkFornecedorComponent {
+  formulario: FormGroup;
+  fornecedores: any[] = []; // Atualizado para 'fornecedores'
+  fornecedorParaEditar: any = null; // Inicialize como null para não preencher o formulário por padrão
 
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      empresa: [''],
+      telefone: [''],
+      email: [''],
+      totalCompras: [''],
+    });
+
+    // Adicione alguns fornecedores à lista como exemplo
+    this.fornecedores.push(
+      {
+        empresa: 'Fornecedor 1',
+        telefone: '123456789',
+        email: 'fornecedor1@example.com',
+        totalCompras: 5000,
+     
+      },
+      {
+        empresa: 'Fornecedor 2',
+        telefone: '987654321',
+        email: 'fornecedor2@example.com',
+        totalCompras: 8000,
+  
+      }
+      // Adicione mais fornecedores se necessário
+    );
+  }
+
+  editarFornecedor(index: number) {
+    this.fornecedorParaEditar = this.fornecedores[index];
+    this.formulario.patchValue(this.fornecedorParaEditar);
+  }
+
+  enviarFormulario() {
+    if (this.formulario.valid) {
+      if (this.fornecedorParaEditar !== null) {
+        const index = this.fornecedores.indexOf(this.fornecedorParaEditar);
+        this.fornecedores[index] = this.formulario.value;
+        this.fornecedorParaEditar = null;
+      } else {
+        this.fornecedores.push(this.formulario.value);
+      }
+      this.formulario.reset();
+    }
+  }
+
+  excluirFornecedor(index: number) {
+    this.fornecedores.splice(index, 1);
+  }
 }
--- MenuInicial/Projeto1/src/app/components/pages/almoxarifado/menu-principal/cadastro/links/link-loja/link-loja.component.ts
+++ MenuInicial/Projeto1/src/app/components/pages/almoxarifado/menu-principal/cadastro/links/link-loja/link-loja.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'loja',
   templateUrl: './link-loja.component.html',
   styleUrls: ['./link-loja.component.css']
 })
 export class LinkLojaComponent {
+  formulario: FormGroup;
+  lojas: any[] = []; // Lista de lojas
+  lojaParaEditar: any = null; // Loja a ser editada (inicializada como null)
 
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      loja: [''],
+      telefone: [''],
+      email: [''],
+      endereco: [''],
+      totalVendido: ['']
+    });
+
+    // Exemplo: Adicionar algumas lojas à lista
+    this.lojas.push(
+      {
+        loja: 'Loja A',
+        telefone: '111-111-1111',
+        email: 'lojaA@example.com',
+        endereco: 'Rua Principal, 123',
+        totalVendido: 10000
+      },
+      {
+        loja: 'Loja B',
+        telefone: '222-222-2222',
+        email: 'lojaB@example.com',
+        endereco: 'Avenida Secundária, 456',
+        totalVendido: 15000
+      }
+      // Adicione mais lojas se necessário
+    );
+  }
+
+  editarLoja(index: number) {
+    this.lojaParaEditar = this.lojas[index];
+    this.formulario.patchValue(this.lojaParaEditar);
+  }
+
+  enviarFormulario() {
+    if (this.formulario.valid) {
+      if (this.lojaParaEditar !== null) {
+        const index = this.lojas.indexOf(this.lojaParaEditar);
+        this.lojas[index] = this.formulario.value;
+        this.lojaParaEditar = null;
+      } else {
+        this.lojas.push(this.formulario.value);
+      }
+      this.formulario.reset();
+    }
+  }
+
+  excluirLoja(index: number) {
+    this.lojas.splice(index, 1);
+  }
 }
