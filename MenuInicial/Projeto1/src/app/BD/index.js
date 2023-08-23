cd56b14f09a7a653ced9c49fc420f2926a2f53d9
Matheus Santos Guedes <matheussantosguedes99@gmail.com>
2023-08-23 10:08:35 -0300

    23-08

--- MenuInicial/Projeto1/src/app/BD/index.js
+++ MenuInicial/Projeto1/src/app/BD/index.js
@@ -24,6 +24,110 @@ db.connect((err) => {
   console.log('Conexão bem-sucedida ao banco de dados!');
 });
 
+app.get('/api/produtos', (req, res) => {
+  db.query('SELECT * FROM cadastrodeprodutos ORDER BY id DESC', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.post('/api/produtos', (req, res) => {
+  const novoProduto = req.body;
+  db.query('INSERT INTO cadastrodeprodutos SET ?', novoProduto, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.put('/api/produtos/:id', (req, res) => {
+  const id = req.params['id'];
+  const produtoAtualizado = req.body;
+  db.query('UPDATE cadastrodeprodutos SET ? WHERE id = ?', [produtoAtualizado, id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.delete('/api/produtos/:id', (req, res) => {
+  const id = req.params['id'];
+  db.query('DELETE FROM cadastrodeprodutos WHERE id = ?', id, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+
+
+
+
+app.get('/api/lojas', (req, res) => {
+  db.query('SELECT * FROM cadastrodelojas ORDER BY id DESC', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.post('/api/lojas', (req, res) => {
+  const novoLoja = req.body;
+  db.query('INSERT INTO cadastrodelojas SET ?', novoLoja, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.put('/api/lojas/:id', (req, res) => {
+  const id = req.params['id'];
+  const lojaAtualizado = req.body;
+  db.query('UPDATE cadastrodelojas SET ? WHERE id = ?', [lojaAtualizado, id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.delete('/api/lojas/:id', (req, res) => {
+  const id = req.params['id'];
+  db.query('DELETE FROM cadastrodelojas WHERE id = ?', id, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+
+
+
+
+app.get('/api/fornecedores', (req, res) => {
+  db.query('SELECT * FROM cadastrodefornecedores ORDER BY id DESC', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+app.post('/api/fornecedores', (req, res) => {
+  const novoLoja = req.body;
+  db.query('INSERT INTO cadastrodefornecedores SET ?', novoLoja, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.put('/api/fornecedores/:id', (req, res) => {
+  const id = req.params['id'];
+  const lojaAtualizado = req.body;
+  db.query('UPDATE cadastrodefornecedores SET ? WHERE id = ?', [lojaAtualizado, id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.delete('/api/fornecedores/:id', (req, res) => {
+  const id = req.params['id'];
+  db.query('DELETE FROM cadastrodefornecedores WHERE id = ?', id, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+
+
+
 app.get('/api/tarefas', (req, res) => {
   db.query('SELECT * FROM tarefas ORDER BY id DESC', (err, results) => {
     if (err) throw err;
